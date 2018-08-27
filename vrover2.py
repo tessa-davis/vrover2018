d66003f9af374f58763b6414c07c60f9410e1baa
Tessa <davist@vmware.com>
2018-08-27 03:01:10 +0000

    Updated vrover2.py with 4 wheel independence.

--- vrover2.py
+++ vrover2.py
@@ -2,42 +2,62 @@ import RPi.GPIO as gpio
 import time
 
 #Goal: Spin wheels forward and reverse
-#Colour is the colour on breadboard
-#On HBridge, connections are to:
-#Assumption: Rt Wheels and Left wheels spin together
-#Blue to Input 4 - Right Reverse Spin - right_rev
-#Purple to Input 3 - Right Forward Spin - right_fwd
-#Grey to Input 2 - Left Forward Spin - left_fwd
-#White to Input 1 - Left Reverse Spin - left_rev
-#Future work: consider 4 wheel independence
+#Assumption: 4 Wheel Independence
+#Refer to test image - colour is lead colour. GPIO input defined in variables:
+#Blue Lead goes to right front wheel- forward spin (rt_fr_fwd) - green LED
+#White lead goes to right front wheel- reverse spin (rt_fr_rev) - red LED
+#Pink lead goes to right back wheel- forward spin (rt_bk_fwd) - green LED
+#Grey lead goes to right back wheel- reverse spin (rt_fr_rev) - red LED
+#Purple Lead goes to left front wheel- forward spin (lft_fr_fwd) - yellow LED
+#Yellow lead goes to left front wheel- reverse spin (lft_fr_rev) - red LED
+#Green lead goes to left back wheel- forward spin (lft_bk_fwd) - yellow LED
+#Orange lead goes to left back wheel- reverse spin (lft_fr_rev) - red LED
 
-right_rev = 7
-right_fwd= 11
-left_fwd = 13
-left_rev = 15
+rt_fr_fwd = 16
+rt_fr_rev = 18
+rt_bk_fwd = 36
+rt_bk_rev = 38
+lft_fr_fwd = 13
+lft_fr_rev = 15
+lft_bk_fwd = 35
+lft_bk_rev = 37
 
 def init():
     gpio.setmode(gpio.BOARD)
-    gpio.setup(right_rev, gpio.OUT)
-    gpio.setup(right_fwd, gpio.OUT)
-    gpio.setup(left_fwd, gpio.OUT)
-    gpio.setup(left_rev, gpio.OUT)
+    gpio.setup(rt_fr_fwd, gpio.OUT)
+    gpio.setup(rt_fr_rev, gpio.OUT)
+    gpio.setup(lft_fr_fwd, gpio.OUT)
+    gpio.setup(lft_fr_rev, gpio.OUT)
+    gpio.setup(rt_bk_fwd, gpio.OUT)
+    gpio.setup(rt_bk_rev, gpio.OUT)
+    gpio.setup(lft_bk_fwd, gpio.OUT)
+    gpio.setup(lft_bk_rev, gpio.OUT)
     
 def forward(tf):
     init()
-    gpio.output(right_rev, False)
-    gpio.output(right_fwd, True)
-    gpio.output(left_fwd, True)
-    gpio.output(left_rev, False)
+    gpio.setmode(gpio.BOARD)
+    gpio.output(rt_fr_fwd, True)
+    gpio.output(rt_fr_rev, False)
+    gpio.output(lft_fr_fwd, True)
+    gpio.output(lft_fr_rev, False)
+    gpio.output(rt_bk_fwd, True)
+    gpio.output(rt_bk_rev, False)
+    gpio.output(lft_bk_fwd, True)
+    gpio.output(lft_bk_rev, False)
+
     time.sleep(tf)
     gpio.cleanup()
     
 def reverse(tf):
     init()
-    gpio.output(right_rev, True)
-    gpio.output(right_fwd, False)
-    gpio.output(left_fwd, False)
-    gpio.output(left_rev, True)
+    gpio.output(rt_fr_fwd, False)
+    gpio.output(rt_fr_rev, True)
+    gpio.output(lft_fr_fwd, False)
+    gpio.output(lft_fr_rev, True)
+    gpio.output(rt_bk_fwd, False)
+    gpio.output(rt_bk_rev, True)
+    gpio.output(lft_bk_fwd, False)
+    gpio.output(lft_bk_rev, True)
     time.sleep(tf)
     gpio.cleanup()
     
